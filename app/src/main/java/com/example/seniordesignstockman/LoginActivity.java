5331fafc11262bd02884c7b39c7ebabe7551c9e1
Meric Mutlu <mericmutlu@yahoo.com>
2022-11-19 21:45:02 +0300

    s

--- app/src/main/java/com/example/seniordesignstockman/LoginActivity.java
+++ app/src/main/java/com/example/seniordesignstockman/LoginActivity.java
@@ -1,18 +1,26 @@
 package com.example.seniordesignstockman;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText Email;
+    private EditText eMail;
     private EditText Password;
     private Button Login;
     private TextView passwordreset;
@@ -26,5 +34,81 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        eMail = findViewById(R.id.emailSignIn);
+        Password = findViewById(R.id.password);
+        Login = findViewById(R.id.login);
+
+        passwordreset = findViewById(R.id.forgotPassword);
+        passwordresetemail = findViewById(R.id.emailSignIn);
+        progressBar = (ProgressBar) findViewById(R.id.progressBars);
+        progressBar.setVisibility(View.GONE);
+        auth = FirebaseAuth.getInstance();
+        processDialog = new ProgressDialog(this);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                try  {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                } catch (Exception e) {
+
+                }
+                validate(eMail.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        passwordreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetPasword();
+            }
+        });
+
+    }
+    public void resetPasword(){
+        final String resetemail = passwordresetemail.getText().toString();
+
+        if (resetemail.isEmpty()) {
+            passwordresetemail.setError("Boş olamaz!");
+            passwordresetemail.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        auth.sendPasswordResetEmail(resetemail).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Hesabınızı nasıl kurtarabileceğinize dair yönerge gönderilmiştir!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Mesaj gönderimi başarısız oldu!", Toast.LENGTH_SHORT).show();
+                }
+
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void validate(String userEmail, String userPassword){
+
+        processDialog.setMessage("................Please Wait.............");
+        processDialog.show();
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    processDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Login Failed", Toast.LENGTH_SHORT).show();
+                    processDialog.dismiss();
+                }
+            }
+        });
     }
 }
